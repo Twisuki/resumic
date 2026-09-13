3b26a1155222ea7fe9d3ec2165988a493bf9d21b
Twisuki <twisuki@outlook.com>
2026-09-14 00:30:52 +0800

    fix: 修复图片资源安全问题

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next"
+import type { PHASE_TYPE } from "next/constants"
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants"
 
-const nextConfig: NextConfig = {
+export default (phase: PHASE_TYPE): NextConfig => ({
   images: {
+    dangerouslyAllowLocalIP: phase === PHASE_DEVELOPMENT_SERVER,
     remotePatterns: [
       { protocol: "https", hostname: "github.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
     ],
   },
-}
-
-export default nextConfig
+})
